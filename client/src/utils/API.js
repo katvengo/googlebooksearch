9833212c28557d6f1b8df379b27642aaa63d465f
Kat Vengo <katvengo@gmail.com>
2019-07-15 22:32:05 -0700

    removed API key

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,10 +1,9 @@
 
-const APIKEY = AIzaSyA_h3gv4yQW7oofZttrlE7xSbQqbxeh72E
 
-const BASEURL = AIzaSyA_h3gv4yQW7oofZttrlE7xSbQqbxeh72E
 
 export default {
     search: function(query) {
-      return get(BASEURL + query + APIKEY);
+      return get(`https://www.googleapis.com/books/v1/volumes?q=search+${query}`
+        );
     }
   };
